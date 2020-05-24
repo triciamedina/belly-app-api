e54f62a4daf25f743d274c9ac5d67ae8d1fa813f
triciamedina <tricia.medina@gmail.com>
2020-05-24 13:14:10 -0700

    Testing ws setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const app = require('./app');
-const websocket = require('./websocket/websocket');
+// const websocket = require('./websocket/websocket');
+const { Server } = require('ws');
 const { PORT, WS_PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
@@ -9,14 +10,29 @@ const db = knex({
 });
 
 app.set('db', db);
-app.set('ws', websocket);
+// app.set('ws', websocket);
 
-websocket.listen('polar-wildwood-68922.herokuapp.com', PORT, (listenSocket) => {
-    if (listenSocket) {
-        console.info(`Websocket listening to port ${PORT}`);
-    }
-})
+// websocket.listen('polar-wildwood-68922.herokuapp.com', PORT, (listenSocket) => {
+//     if (listenSocket) {
+//         console.info(`Websocket listening to port ${PORT}`);
+//     }
+// })
+
+
 
 app.listen(PORT, () => {
     console.info(`Http listening at http://localhost:${PORT}`);
 });
+
+const wss = new Server({ app });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+  });
+  
+  setInterval(() => {
+    wss.clients.forEach((client) => {
+      client.send(new Date().toTimeString());
+    });
+  }, 1000);
