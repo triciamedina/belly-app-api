38fdf71c58f0557f17970b6f0e8a57ac2bc83c1e
triciamedina <tricia.medina@gmail.com>
2020-05-23 19:06:07 -0700

    Test reconfigure server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const app = require('./app');
-const websocket = require('./websocket/websocket');
+
 const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
@@ -11,11 +11,16 @@ const db = knex({
 app.set('db', db);
 app.set('ws', websocket);
 
-websocket.listen(PORT, (listenSocket) => {
-    if (listenSocket) {
-        console.info(`Websocket listening to port ${PORT}`);
-    }
-})
+// const app = require('../app');
+// const server = require('http').createServer(app);
+const websocket = require('./websocket/websocket').listen(app);
+// const uWS = require('uWebSockets.js').listen(server);
+
+// websocket.listen(PORT, (listenSocket) => {
+//     if (listenSocket) {
+//         console.info(`Websocket listening to port ${PORT}`);
+//     }
+// })
 
 app.listen(PORT, () => {
     console.info(`Http listening at http://localhost:${PORT}`);
