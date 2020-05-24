528e7b47c760cd68bdd561bfc3e78a75ec87f8b4
triciamedina <tricia.medina@gmail.com>
2020-05-24 13:16:29 -0700

    Update server config

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.listen(PORT, () => {
     console.info(`Http listening at http://localhost:${PORT}`);
 });
 
-const wss = new Server({ app });
+const wss = new Server({ server: app });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
