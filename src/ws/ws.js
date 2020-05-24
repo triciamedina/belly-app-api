948080dbd80522f61a153f1f8f8b4d3b3a94e1b3
triciamedina <tricia.medina@gmail.com>
2020-05-24 16:13:04 -0700

    Test message for user entry

--- src/ws/ws.js
+++ src/ws/ws.js
@@ -6,9 +6,27 @@ wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', function incoming(message) {
-      console.log('received: %s', message);
+      const activity = JSON.parse(message);
+      console.log('received: ', message);
+
+      if (activity.newUser) {
+        console.log('new user entered')
+        // const id = WebSocketService.getUniqueID();
+
+        // if (!clients[billId]) {
+        //     clients[billId] = {}
+        // }
+
+        // const username = activity.newUser.nickname;
+        // clients[billId][username] = activity.newUser;
+
+        // ws.send(JSON.stringify({ viewerJoined: true, id: id }));
+
+        // ws.publish(`bill/${billId}/users`, JSON.stringify({ updateViewers: true, clients: clients[billId] }));
+        // console.log(clients)
+      }
     });
-    
+
     ws.on('close', () => console.log('Client disconnected'));
   });
 
