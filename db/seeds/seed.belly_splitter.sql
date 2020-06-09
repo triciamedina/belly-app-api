135137f62d5de366637712e3d429bcec2270235f
triciamedina <tricia.medina@gmail.com>
2020-06-09 13:51:04 -0700

    Update seed scripts with hex values for avatar

--- db/seeds/seed.belly_splitter.sql
+++ db/seeds/seed.belly_splitter.sql
@@ -6,7 +6,7 @@ TRUNCATE
 
 INSERT INTO belly_splitter (nickname, avatar, created_at)
 VALUES
-    ('Sam', 'blue', '2020-01-02T23:28:56.782Z'),
-    ('Frodo', 'pink', '2020-01-03T23:28:56.782Z');
+    ('Sam', '#405cf7', '2020-01-02T23:28:56.782Z'),
+    ('Frodo', '#ca03a3', '2020-01-03T23:28:56.782Z');
 
 COMMIT;
--- db/seeds/seed.belly_user.sql
+++ db/seeds/seed.belly_user.sql
@@ -6,7 +6,7 @@ TRUNCATE
 
 INSERT INTO belly_user (username, avatar, password)
 VALUES
-    ('Sam', 'blue', '$2a$12$P.fk9nIrAHizny9LaWtBEucNmJAXsXy5MmM6twpEZsRAa6/7Cjisy'),
-    ('Frodo', 'pink', '$2a$12$a1gMxChnEZcZT7bZO/5qxOS/ZzX3OZDdBpzxvGA5XQ6fEyqHHTbeK');
+    ('Sam', '#405cf7', '$2a$12$P.fk9nIrAHizny9LaWtBEucNmJAXsXy5MmM6twpEZsRAa6/7Cjisy'),
+    ('Frodo', '#ca03a3', '$2a$12$a1gMxChnEZcZT7bZO/5qxOS/ZzX3OZDdBpzxvGA5XQ6fEyqHHTbeK');
 
 COMMIT;
