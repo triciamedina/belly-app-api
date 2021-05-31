daccb6445e83d810d948a8c28e917a017e0ab19c
Tricia Medina <48637126+triciamedina@users.noreply.github.com>
2021-05-31 16:05:39 -0700

    Reset CORS

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors({
-    // origin: CLIENT_ORIGIN
-    origin: '*'
+    origin: CLIENT_ORIGIN
 }));
 
 app.use('/api/auth', authRouter);
